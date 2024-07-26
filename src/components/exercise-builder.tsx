3372b3a879039946366ce07babea266f34ab0895
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-07-26 00:17:03 -0400

    Form test

--- src/components/exercise-builder.tsx
+++ src/components/exercise-builder.tsx
@@ -1,3 +1,49 @@
+import { createForm } from "@modular-forms/solid";
+import { Button, Form } from "solid-bootstrap";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
 export default function ExerciseBuilder() {
-  return <></>;
+  const [loginForm, Login] = createForm<LoginForm>();
+  const { Field } = Login;
+
+  return (
+    <>
+      Form Test:
+      <Login.Form
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Field name="email">
+          {(field, props) => (
+            <Form.Group class="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                {...props}
+                type="email"
+                placeholder="name@example.com"
+              />
+            </Form.Group>
+          )}
+        </Field>
+        <Field name="password">
+          {(field, props) => (
+            <Form.Group class="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                {...props}
+                type="password"
+                placeholder="name@example.com"
+              />
+            </Form.Group>
+          )}
+        </Field>
+        <Button type="submit">Generate</Button>
+      </Login.Form>
+    </>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
+import { Container } from "solid-bootstrap";
 import ExerciseBuilder from "~/components/exercise-builder";
 
 export default function Home() {
   return (
     <main>
-      <h1>Sight Reading Trainer</h1>
-      <ExerciseBuilder />
+      <Container>
+        <h1>Sight Reading Trainer</h1>
+        <ExerciseBuilder />
+      </Container>
     </main>
   );
 }
