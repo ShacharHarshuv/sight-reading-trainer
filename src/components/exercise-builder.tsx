d87a6bfbe8721a4b78dcd8908e0a9724cca8b74b
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-07-26 22:30:33 -0400

    form test

--- src/components/exercise-builder.tsx
+++ src/components/exercise-builder.tsx
@@ -1,49 +1,81 @@
-import { createForm } from "@modular-forms/solid";
-import { Button, Form } from "solid-bootstrap";
+import { createForm } from "@tanstack/solid-form";
+import { Form } from "solid-bootstrap";
 
-type LoginForm = {
-  email: string;
-  password: string;
-};
+function toNumber(value: string) {
+  if (!value) {
+    return undefined;
+  }
+  const parsed = Number(value);
+  return isNaN(parsed) ? undefined : parsed;
+}
 
 export default function ExerciseBuilder() {
-  const [loginForm, Login] = createForm<LoginForm>();
-  const { Field } = Login;
+  const form = createForm(() => ({
+    defaultValues: {
+      name: "",
+      age: undefined as number | undefined,
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log("submit", value);
+    },
+  }));
+
+  const canSubmit = form.useStore((state) => state.canSubmit);
 
   return (
-    <>
-      Form Test:
-      <Login.Form
-        onSubmit={(values) => {
-          console.log(values);
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
         }}
       >
-        <Field name="email">
-          {(field, props) => (
-            <Form.Group class="mb-3">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                {...props}
-                type="email"
-                placeholder="name@example.com"
-              />
-            </Form.Group>
-          )}
-        </Field>
-        <Field name="password">
-          {(field, props) => (
-            <Form.Group class="mb-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                {...props}
-                type="password"
-                placeholder="name@example.com"
-              />
-            </Form.Group>
-          )}
-        </Field>
-        <Button type="submit">Generate</Button>
-      </Login.Form>
-    </>
+        <div>
+          <form.Field name="name">
+            {(field) => (
+              <Form.Group class="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  name={field().name}
+                  value={field().state.value}
+                  onBlur={field().handleBlur}
+                  onInput={(e) => field().setValue(e.currentTarget.value)}
+                />
+              </Form.Group>
+            )}
+          </form.Field>
+          <form.Field
+            name="age"
+            validators={{
+              onChange: ({ value }) =>
+                value === undefined ? "Age is required" : undefined,
+            }}
+          >
+            {(field) => (
+              <Form.Group class="mb-3" controlId="formBasicEmail">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  type="number"
+                  name={field().name}
+                  value={field().state.value}
+                  onBlur={field().handleBlur}
+                  onInput={(e) =>
+                    field().setValue(toNumber(e.currentTarget.value))
+                  }
+                />
+                {field().state.meta.errors ? (
+                  <em role="alert">{field().state.meta.errors.join(", ")}</em>
+                ) : null}
+              </Form.Group>
+            )}
+          </form.Field>
+        </div>
+        <button disabled={!canSubmit()} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
